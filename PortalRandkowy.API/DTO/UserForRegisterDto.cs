bf1451235c7620c2ca64f322d6c237851be50345
Maciej Pomo <maciejpomo@gmail.com>
2020-03-31 17:19:26 +0200

    Dodanie rejestracji

---
+++ PortalRandkowy.API/DTO/UserForRegisterDto.cs
@@ -0,0 +1,9 @@
+namespace PortalRandkowy.API.DTO
+{
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
