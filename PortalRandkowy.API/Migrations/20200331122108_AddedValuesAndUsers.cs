05ccdfa7d13edec415f2019086cff150ba96c811
Maciej Pomo <maciejpomo@gmail.com>
2020-03-31 14:46:22 +0200

    Dodanie repozytorium

--- PortalRandkowy.API/Migrations/20200331122108_AddedValuesAndUsers.cs
+++ PortalRandkowy.API/Migrations/20200331122108_AddedValuesAndUsers.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace PortalRandkowy.API.Migrations
 {
-    public partial class AddedUser : Migration
+    public partial class AddedValuesAndUsers : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -13,8 +14,8 @@ namespace PortalRandkowy.API.Migrations
                     Id = table.Column<int>(nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
                     Username = table.Column<string>(nullable: true),
-                    PasswordHash = table.Column<byte>(nullable: false),
-                    PasswordSalt = table.Column<byte>(nullable: false)
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true)
                 },
                 constraints: table =>
                 {
--- PortalRandkowy.API/Models/User.cs
+++ PortalRandkowy.API/Models/User.cs
@@ -6,8 +6,8 @@ namespace PortalRandkowy.API.Models
 
         public string Username { get; set; }
 
-        public byte PasswordHash { get; set; }
+        public byte[] PasswordHash { get; set; }
 
-        public byte PasswordSalt { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
